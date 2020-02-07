abe6f7233612f65f8e82a36ab3d5c878112cb3c3
Mayck Xavier <mayckxavier@gmail.com>
2020-02-07 08:57:44 -0300

    Test if number is less than 999999

--- zip_validator/__init__.py
+++ zip_validator/__init__.py
@@ -1,5 +1,7 @@
 def validate(number):
-	if number > 100000:
-		return True
+	if number <= 100000:
+		return False
+	elif number >= 999999:
+		return False
 
-	return False
+	return True
--- zip_validator_test.py
+++ zip_validator_test.py
@@ -3,20 +3,25 @@ from zip_validator import validate
 
 
 class ZipValidatorTest(unittest.TestCase):
-	
-	def test_number_must_be_greater_than_100000(self):
-		"""Number must be greater than 100000"""
-		
-		min_value = validate(100000)
-		self.assertEqual(min_value, False)
+    def test_number_must_be_greater_than_minimum(self):
+        """Number must be greater than 100000"""
 
-		min_value = validate(100001)
-		self.assertEqual(min_value, True)
+        is_valid = validate(100000)
+        self.assertEqual(is_valid, False)
 
+        is_valid = validate(100001)
+        self.assertEqual(is_valid, True)
 
+    def test_number_must_be_less_then_max_allowed(self):
+        is_valid = validate(999999)
+        self.assertEqual(is_valid, False)
 
+        is_valid = validate(9999991)
+        self.assertEqual(is_valid, False)
 
+        is_valid = validate(999998)
+        self.assertEqual(is_valid, True)
 
 
 if __name__ == '__main__':
-	unittest.main()
+    unittest.main()

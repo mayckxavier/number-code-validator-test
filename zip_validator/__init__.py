ca7aee43afa8967d16860c6b3ef8b10aa6c643f9
Mayck Xavier <mayckxavier@gmail.com>
2020-02-07 09:26:00 -0300

    Testing if number is has a valid value

--- zip_validator/__init__.py
+++ zip_validator/__init__.py
@@ -1,7 +1,14 @@
-def validate(number):
-	if number <= 100000:
-		return False
-	elif number >= 999999:
-		return False
+import re
 
-	return True
+
+def validate(number):
+    p = re.compile('^[0-9]{6,6}$')
+    if not p.match(str(number)):
+        return False
+
+    if number <= 100000:
+        return False
+    elif number >= 999999:
+        return False
+
+    return True
--- zip_validator_test.py
+++ zip_validator_test.py
@@ -3,6 +3,30 @@ from zip_validator import validate
 
 
 class ZipValidatorTest(unittest.TestCase):
+    def test_must_be_a_valid_number(self):
+        """Number must be a valid value"""
+
+        is_valid = validate(100001)
+        self.assertEqual(is_valid, True)
+
+        is_valid = validate(999995)
+        self.assertEqual(is_valid, True)
+
+    def test_must_be_an_invalid_number(self):
+        """Number must be an invalid value"""
+
+        is_valid = validate('jose')
+        self.assertEqual(is_valid, False)
+
+        is_valid = validate(1000001)
+        self.assertEqual(is_valid, False)
+
+        is_valid = validate(9999991)
+        self.assertEqual(is_valid, False)
+
+        is_valid = validate(100)
+        self.assertEqual(is_valid, False)
+
     def test_number_must_be_greater_than_minimum(self):
         """Number must be greater than 100000"""
 
